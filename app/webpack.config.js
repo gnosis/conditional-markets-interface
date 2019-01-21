a9ea6857b4e48f129d90da71c7be223dc41f1839
Andre Meyer <andre.meyer93@gmail.com>
2019-01-21 14:23:06 +0100

    hotfix github pages

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: __dirname + "/../docs",
-    publicPath: "/",
+    publicPath: "/hg-first-decentralized-market/",
     filename: "bundle.js"
   },
   resolve: {
