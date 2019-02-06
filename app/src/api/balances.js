a82aabaf4a34d3f394e1a6bc0f72e3298155554a
Andre Meyer <andre.meyer93@gmail.com>
2019-02-06 12:28:21 +0100

    implement balances correctly, taking a sum of all positions that have a stake in the current outcome index

--- app/src/api/balances.js
+++ app/src/api/balances.js
@@ -1,5 +1,10 @@
+import web3 from 'web3'
+
 import { getDefaultAccount, loadContract } from "./web3";
 import { generatePositionId } from "./utils/positions";
+import { resolvePartitionSets } from "./utils/probabilities"
+
+const { toBN, BN } = web3.utils
 
 export const retrieveBalances = async (pmsystem, markets, _positionIds) => {
   const owner = await getDefaultAccount();
@@ -23,7 +28,6 @@ export const retrieveBalances = async (pmsystem, markets, _positionIds) => {
             positionIds.push(
               generatePositionId(markets, collateral, lmsrOutcomeIndex)
             );
-            //console.log(lmsrOutcomeIndex)
             lmsrOutcomeIndex++;
           });
       })
@@ -33,8 +37,7 @@ export const retrieveBalances = async (pmsystem, markets, _positionIds) => {
 
   // get position balances
   const balances = {};
-
-  const balancePromises = Promise.all(
+  await Promise.all(
     positionIds.map(async positionId => {
       balances[positionId] = (await pmsystem.balanceOf(
         owner,
@@ -43,7 +46,34 @@ export const retrieveBalances = async (pmsystem, markets, _positionIds) => {
     })
   );
 
-  await balancePromises;
+  // map balances to outcome positions
 
-  return balances;
+  // create an empty market structure like [[0, 0, 0], [0, 0, 0]] to map position combinations
+  const marketStructure = markets.map((market) => market.outcomes.map(() => 0))
+  const { outcomeIds, outcomePairs } = resolvePartitionSets(marketStructure)
+
+  const outcomeBalances = marketStructure.map((marketOutcomePrices) => marketOutcomePrices.map(() => new BN(0)))
+
+  // loop through positionIds, matching their corresponding outcomeIndexes to outcomePairs
+  // and then suming them the corresponding outcomeIds
+  positionIds.forEach((positionId, outcomeIndex) => {
+    const outcomePair = outcomePairs.flat()[outcomeIndex]
+
+    console.log(`finding balance for ${outcomePair}`)
+
+    outcomeBalances.forEach((marketOutcomePrices, outcomeGroupIndex) => {
+      marketOutcomePrices.forEach((_, priceIndex) => {
+        const outcomeId = outcomeIds[outcomeGroupIndex][priceIndex]
+        
+        if (outcomePair.indexOf(outcomeId) > -1) {
+          const amount = new BN(balances[positionId])
+          outcomeBalances[outcomeGroupIndex][priceIndex] = 
+            outcomeBalances[outcomeGroupIndex][priceIndex].add(amount)
+        }
+      })
+    })
+  })
+  console.log(outcomeBalances)
+
+  return outcomeBalances;
 };
