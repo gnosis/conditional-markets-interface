8da3c56c4a7719e3b9f320bd9a1374e47b72b6dd
Andre Meyer <andre.meyer93@gmail.com>
2019-03-05 13:55:55 +0100

    hardcode dai symbol/name/decimals

--- app/src/api/markets.js
+++ app/src/api/markets.js
@@ -121,6 +121,14 @@ export const loadCollateral = async () => {
     }
   }
 
+  if (collateral.toLowerCase() === "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359") {
+    return {
+      symbol: "DAI",
+      decimals: 18,
+      name: "MakerDAO Dai Stablecoin"
+    }
+  }
+
   const erc20 = await loadContract("ERC20Detailed", collateral);
   const symbol = await erc20.symbol()
   const name = await erc20.name()
