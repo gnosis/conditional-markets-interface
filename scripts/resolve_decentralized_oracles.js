201474e1ae3011db74e72978ce7081eb561935ea
Alan Lu <alanlu1023@gmail.com>
2019-04-26 16:20:05 -0500

    tweak decentralized oracle cli to match markets order in interface

--- scripts/resolve_decentralized_oracles.js
+++ scripts/resolve_decentralized_oracles.js
@@ -1,6 +1,6 @@
 const DifficultyOracle = artifacts.require("DifficultyOracle");
-const ETHValueOracle = artifacts.require("ETHValueOracle");
 const GasLimitOracle = artifacts.require("GasLimitOracle");
+const ETHValueOracle = artifacts.require("ETHValueOracle");
 
 const inquirer = require("inquirer");
 
@@ -11,8 +11,8 @@ module.exports = callback => {
     let gasLimitOracleInstance;
     try {
       difficultyOracleInstance = await DifficultyOracle.deployed();
-      ethValueOracleInstance = await ETHValueOracle.deployed();
       gasLimitOracleInstance = await GasLimitOracle.deployed();
+      ethValueOracleInstance = await ETHValueOracle.deployed();
     } catch (err) {
       console.error("Please ensure the oracle contracts are deployed.");
       console.error(err.message);
@@ -24,10 +24,10 @@ module.exports = callback => {
       `Difficult Oracle deployed at ${difficultyOracleInstance.address}`
     );
     console.log(
-      `ETH Value Oracle deployed at ${ethValueOracleInstance.address}`
+      `Gas Limit Oracle deployed at ${gasLimitOracleInstance.address}`
     );
     console.log(
-      `Gas Limit Oracle deployed at ${gasLimitOracleInstance.address}`
+      `ETH Value Oracle deployed at ${ethValueOracleInstance.address}`
     );
 
     const answers = await inquirer.prompt([
@@ -37,31 +37,32 @@ module.exports = callback => {
         message: "Which oracle do you want to resolve?",
         choices: [
           { value: "Difficulty", name: "Difficulty Oracle" },
-          { value: "ETHValue", name: "ETH Value Oracle" },
-          { value: "GasLimit", name: "Gas Limit Oracle" }
+          { value: "GasLimit", name: "Gas Limit Oracle" },
+          { value: "ETHValue", name: "ETH Value Oracle" }
         ]
       }
     ]);
 
     console.log("Resolving...");
-    let tx;
+
     try {
       if (answers.oracle == "Difficulty") {
-        tx = await difficultyOracleInstance.resolveDifficulty();
-      } else if (answers.oracle == "ETHValue") {
-        tx = await ethValueOracleInstance.resolveETHValue();
+        await difficultyOracleInstance.resolveDifficulty();
       } else if (answers.oracle == "GasLimit") {
-        tx = await gasLimitOracleInstance.resolveGasLimit();
+        await gasLimitOracleInstance.resolveGasLimit();
+      } else if (answers.oracle == "ETHValue") {
+        await ethValueOracleInstance.resolveETHValue();
       }
     } catch (err) {
       console.error(err);
-      rej(err);
+      return rej(err);
     }
-    console.log(JSON.stringify(tx, null, 2));
 
+    console.log("Resolution successful!");
     res();
   }).then(callback, err => {
     console.log("Oracle Resolve failed");
     console.error(err);
+    callback(err);
   });
 };
