745bccaa97082f37aca7bcb261d139041b882db3
Alan Lu <alanlu1023@gmail.com>
2019-05-23 12:59:06 -0500

    change default resolution time to thirty days from deployment

--- migrations/utils/deploy-config.js
+++ migrations/utils/deploy-config.js
@@ -1,19 +1,20 @@
-const fiveMinutesFromNow = Math.floor(Date.now() / 1000) + 60 * 5;
+// default to 30 days later
+const defaultResolutionTime = Math.floor(Date.now() / 1000) + 30 * 24 * 60 * 60;
 
 module.exports = {
   daiPriceResolutionTime:
-    process.env.DAIPRICERESOLUTIONTIME || fiveMinutesFromNow,
+    process.env.DAIPRICERESOLUTIONTIME || defaultResolutionTime,
   daiPriceTargetValue: process.env.DAIPRICETARGETVALUE || "1" + "0".repeat(18),
   daiPriceQuestionID: process.env.DAIPRICEQUESTIONID || "0x01",
 
   daiSupplyResolutionTime:
-    process.env.DAISUPPLYRESOLUTIONTIME || fiveMinutesFromNow,
+    process.env.DAISUPPLYRESOLUTIONTIME || defaultResolutionTime,
   daiSupplyTargetValue:
     process.env.DAISUPPLYTARGETVALUE || "81000000" + "0".repeat(18),
   daiSupplyQuestionID: process.env.DAISUPPLYQUESTIONID || "0x02",
 
   daiStabilityFeeResolutionTime:
-    process.env.DAISTABILITYFEERESOLUTIONTIME || fiveMinutesFromNow,
+    process.env.DAISTABILITYFEERESOLUTIONTIME || defaultResolutionTime,
   daiStabilityFeeTargetValue:
     process.env.DAISTABILITYFEETARGETVALUE || "1000000005113779426955452540",
   daiStabilityFeeQuestionID: process.env.DAISTABILITYFEEQUESTIONID || "0x03",
