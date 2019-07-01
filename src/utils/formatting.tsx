65fdf778c26306259cfddbbe47f1e6f21454fbe6
bneiluj <julien.bouteloup@gmail.com>
2019-07-01 13:55:11 +0200

    more tsx stuff

---
+++ src/utils/formatting.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Decimal from "decimal.js-light";
+
+import {
+  probabilityDecimalPlaces,
+  collateralSignificantDigits
+} from "./constants";
+
+export const formatProbability = probability =>
+  `${probability
+    .mul(100)
+    .toDecimalPlaces(probabilityDecimalPlaces, Decimal.ROUND_HALF_UP)}%`;
+
+export const formatCollateral = (amount, collateral) => {
+  return `${new Decimal((amount || "0").toString())
+    .mul(collateral.fromUnitsMultiplier)
+    .toSignificantDigits(collateralSignificantDigits)
+    .toString()} ${collateral.symbol}`;
+};
+
+const REPLACEMENT_RULES = [[/_(.*)_/g, "<em>$1</em>"]];
+
+export const pseudoMarkdown = (str: string) => {
+  let parsedString = REPLACEMENT_RULES.reduce(
+    (prevString, [matcher, replacer]) => str.replace(matcher, replacer),
+    str
+  );
+
+  return <span dangerouslySetInnerHTML={{ __html: parsedString }} />;
+};
