e1a96e5aefc54000558d891f2d448a353ad103d0
Alan Lu <alanlu1023@gmail.com>
2019-08-22 08:23:43 +0200

    Rinkeby deploy

--- markets.config.js
+++ markets.config.js
@@ -1,55 +1,57 @@
-const resolutionDate = new Date();
-resolutionDate.setDate(resolutionDate.getDate() + 14);
-
 module.exports = [
   {
-    questionId: `0x${"0".repeat(63)}a`,
-    title: `Will the DAI price exceed $1 according to the DutchX last auction closing price and Maker ETH price feed?`,
-    resolutionDate: resolutionDate.toISOString(),
+    questionId:
+      "0xda66c02001000000000000000000000000000000000000000000000000000000",
+    title: "Will Multi-Collateral Dai go live on Mainnet by November 1st 2019?",
+    resolutionDate: "2019-09-01T16:00:00.000Z",
     outcomes: [
       {
         title: "Yes",
         short: "Yes",
-        when: "1 DAI > $1"
+        when: "MCD live Nov 1"
       },
       {
         title: "No",
         short: "No",
-        when: "1 DAI ≤ $1"
+        when: "MCD not live Nov 1"
       }
     ]
   },
   {
-    questionId: `0x${"0".repeat(63)}b`,
-    title: "Will the DAI supply exceed 81 million DAI?",
-    resolutionDate: resolutionDate.toISOString(),
+    questionId:
+      "0xda66c02002000000000000000000000000000000000000000000000000000000",
+    title:
+      "Will Multi-Collateral Dai overtake Single-Collateral Dai by March 31st 2020?",
+    resolutionDate: "2020-03-31T16:00:00.000Z",
     outcomes: [
       {
         title: "Yes",
         short: "Yes",
-        when: "DAI supply > 81 million DAI"
+        when: "MCD is Dai"
       },
       {
         title: "No",
         short: "No",
-        when: "DAI supply ≤ 81 million DAI"
+        when: "SCD is Dai"
       }
     ]
   },
   {
-    questionId: `0x${"0".repeat(63)}c`,
-    title: "Will the DAI stability fee exceed 17.5%?",
-    resolutionDate: resolutionDate.toISOString(),
+    questionId:
+      "0xda66c02003000000000000000000000000000000000000000000000000000000",
+    title:
+      "Will the stability fee for Single-Collateral Dai go up from 20.5% before November 1st 2019?",
+    resolutionDate: "2019-09-01T16:00:00.000Z",
     outcomes: [
       {
         title: "Yes",
         short: "Yes",
-        when: "DAI stability fee > 17.5%"
+        when: "Dai stability fee > 20.5%"
       },
       {
         title: "No",
         short: "No",
-        when: "DAI stability fee ≤ 17.5%"
+        when: "Dai stability fee ≤ 20.5%"
       }
     ]
   }
