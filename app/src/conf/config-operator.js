dccc41dcd0fa803c43342c14d9dace92ce583205
Daniel Sánchez <daniel.sanchez@gnosis.pm>
2019-11-05 13:45:59 +0100

    Add sight operator configuration for staging/development

--- app/src/conf/config-operator.js
+++ app/src/conf/config-operator.js
@@ -1,13 +1,9 @@
 const isProduction = process.env.NODE_ENV === "production";
 
-// const defaultWhitelistEnabled = isProduction ? true : false;
-
-const defaultOperatorApi = "https://sight-operator.dev.gnosisdev.com/api/v1"
-// isProduction
-//   ? "https://sight-operator.staging.gnosisdev.com/api/v1"
-//   : "/api";
+const defaultOperatorApi = isProduction
+  ? "https://sight-operator.staging.gnosisdev.com/api/v1"
+  : "https://sight-operator.dev.gnosisdev.com/api/v1";
 
 module.exports = {
-  // WHITELIST_ENABLED: process.env.WHITELIST_ENABLED || defaultWhitelistEnabled,
   OPERATOR_API_URL: process.env.OPERATOR_API || defaultOperatorApi
 };
