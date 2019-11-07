9bca73e165c858695518b46b108bd0149529e01b
Daniel Sánchez <daniel.sanchez@gnosis.pm>
2019-11-07 10:49:11 +0100

    Fix travis if sentence

--- app/src/conf/config-operator.js
+++ app/src/conf/config-operator.js
@@ -4,6 +4,8 @@ const defaultOperatorApi = isMainnet
   ? "https://sight-operator.staging.gnosisdev.com/api/v1"
   : "https://sight-operator.dev.gnosisdev.com/api/v1";
 
+console.log(process.env.OPERATOR_API);
+
 module.exports = {
   OPERATOR_API_URL: process.env.OPERATOR_API || defaultOperatorApi
 };
