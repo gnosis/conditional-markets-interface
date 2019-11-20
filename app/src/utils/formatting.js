0e963486e0adfe6612d1a85db75123dafedc354c
Andre Meyer <andre.meyer93@gmail.com>
2019-11-20 12:12:32 +0100

    hotfix: formatting issues

--- app/src/utils/formatting.js
+++ app/src/utils/formatting.js
@@ -4,7 +4,9 @@ import Decimal from "decimal.js-light";
 import {
   probabilityDecimalPlaces,
   collateralSignificantDigits,
-  quantitySiginificantDigits
+  quantitySiginificantDigits,
+  collateralConsiderAsZeroDigits,
+  zeroDecimal
 } from "./constants";
 
 const smellsLikeDecimal = val => {
@@ -21,14 +23,24 @@ export const formatCollateral = (amount, collateral) => {
     ? amount
     : new Decimal((amount || "0").toString());
 
-  const minValue = new Decimal(10).pow(-collateralSignificantDigits);
+  const amountFullUnits = amountDecimal.mul(collateral.fromUnitsMultiplier);
 
-  if (amountDecimal.lt(minValue)) {
-    return `<${minValue.toString()}`;
+  const minValue = new Decimal(10).pow(-collateralSignificantDigits);
+  const minValueToConsiderAsZero = new Decimal(10).pow(
+    -collateralConsiderAsZeroDigits
+  );
+
+  if (amountFullUnits.abs().lt(minValueToConsiderAsZero)) {
+    return `0 ${collateral.symbol}`;
   }
 
-  const collateralValue = amountDecimal
-    .mul(collateral.fromUnitsMultiplier)
+  if (amountFullUnits.abs().lt(minValue)) {
+    return `<${
+      amountFullUnits.lt(zeroDecimal) ? " -" : ""
+    }${minValue.toString()} ${collateral.symbol}`;
+  }
+
+  const collateralValue = amountFullUnits
     .toSignificantDigits(collateralSignificantDigits)
     .toString();
   return `${collateralValue} ${collateral.symbol}`;
